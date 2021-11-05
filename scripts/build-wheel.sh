95bb72e0da5dc5c2922a56fb03c997c701db326d
abelsonlive <brian@abelson.live>
2021-11-05 13:56:57 -0400

    cleanup

--- scripts/build-wheel.sh
+++ scripts/build-wheel.sh
@@ -4,4 +4,3 @@ set -ex
 rm -rf dist/
 python setup.py bdist_wheel
 aws s3 sync dist/ s3://parsely-artifacts/lib/wheels/
-curl -H "Content-Type: application/json" -H "X-Auth-Token: $PYPI_WEBHOOK_SECRET" -X POST https://pypi-webhook.cogtree.com/hooks/wheel-aws-sync
